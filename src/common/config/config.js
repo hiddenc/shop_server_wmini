4662735daaf29f9d836065c7b1239b0243cf7415
ch <caobody@gmail.com>
2018-05-25 21:10:31 +0800

    first push

---
+++ src/common/config/config.js
@@ -0,0 +1,17 @@
+// default config
+module.exports = {
+  default_module: 'api',
+  weixin: {
+    appid: 'wxaa25919dd8b6990f', // 小程序 appid
+    secret: 'c3aaf12f5d0cb62ddb7c8d34a46dd3e4', // 小程序密钥
+    mch_id: '', // 商户帐号ID
+    partner_key: '', // 微信支付密钥
+    notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
+  },
+  express: {
+    // 快递物流信息查询使用的是快递鸟接口，申请地址：http://www.kdniao.com/
+    appid: '', // 对应快递鸟用户后台 用户ID
+    appkey: '', // 对应快递鸟用户后台 API key
+    request_url: 'http://api.kdniao.cc/Ebusiness/EbusinessOrderHandle.aspx'
+  }
+};
